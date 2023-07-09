73335f3e15e20739615ab413df71db1bc43a4f17
ChanHee <naya.chanhi@gmail.com>
2023-07-09 18:39:55 +0900

    'COMMIT!

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ english_address_class.english_filepath()
 korean_address_class = file_path(korean_address_cut_unit)
 korean_address_class.korean_filepath()
 
-# 주소 오류 정정
-english_path = english_address_class.english_path
-korean_path = korean_address_class.korean_path
-
-
 # 영문주소를 한글주소로 변환====
 english2korean = english_address_class.English2Korean(english_address_cut_unit)
 korean2english = korean_address_class.Korean2English(korean_address_cut_unit)
